a806385cc5b147c01bcde7fb823b973f2906274c
643473049-star <643473049@qq.com>
2026-08-07 10:02:02 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// 离线缓存：App 壳
+const CACHE = 'quiz-pwa-v1';
+const FILES = [
+  '.',
+  'index.html',
+  'styles.css',
+  'store.js',
+  'parser.js',
+  'app.js',
+  'manifest.webmanifest',
+  'assets/icon-192.png',
+  'assets/icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return res;
+    }).catch(() => hit))
+  );
+});
